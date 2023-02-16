5281139e96319178a4b88ab6f7db7ec8ca1d6231
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-02-16 14:54:27 -0800

    puts instead of printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - used the function puts instead of printf
+ * Return: Always 0
+ */
+int main(void)
+	{
+		puts("Programming is like building a multilingual puzzle");
+
+		return (0);
+	}

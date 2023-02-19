5870cedc92e31f819ca9b3616a5cba63437a8c5e
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-02-19 10:39:31 -0800

    Mixed letters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,7 +9,7 @@ int main(void)
 	char lower_upper;
 
 	for (lower_upper = 'a'; lower_upper <= 'z'; lower_upper++)
-		putchar(lower_case);
+		putchar(lower_upper);
 	for (lower_upper = 'A'; lower_upper <= 'Z'; lower_upper++)
 		putchar(lower_upper);
 	putchar('\n');

ebb202ce30c7d8c155a467f32cd1f8a6c7ab5a12
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-02-19 09:56:29 -0800

    3-print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	char lower_upper;
+
+	for (lower_upper = 'a'; lower_upper <= 'z'; lower_upper++)
+		putchar(lower_case);
+	for (lower_upper = 'A'; lower_upper <= 'z'; lower_upper++)
+		putchar(lower_upper);
+	putchar('\n');
+	return (o);
+}

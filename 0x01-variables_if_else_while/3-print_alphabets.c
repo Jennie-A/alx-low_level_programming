18f001369a4e1a931fe5dd497d922f6eede9ec7a
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-02-19 10:07:07 -0800

    alphaBETS

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (lower_upper = 'a'; lower_upper <= 'z'; lower_upper++)
 		putchar(lower_case);
-	for (lower_upper = 'A'; lower_upper <= 'z'; lower_upper++)
+	for (lower_upper = 'A'; lower_upper <= 'Z'; lower_upper++)
 		putchar(lower_upper);
 	putchar('\n');
 	return (o);

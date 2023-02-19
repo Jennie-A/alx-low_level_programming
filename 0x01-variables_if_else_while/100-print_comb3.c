83e1f791f8631910030ba60454af2ec6c5cbdbdd
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-02-19 14:11:05 -0800

    Possible different combination of two numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - All possible combination of two digits
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int fist;
+	int second;
+
+	for (first = 0; first < 9; first++)
+	{
+		for (second = first + 1; second < 10; second++)
+		{
+			putchar('0' + first);
+			putchar('0' + second);
+
+			if (first == 8 && second == 9)
+				continue;
+			putchar(' ');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

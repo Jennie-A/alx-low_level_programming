ee9e74800c0d04933b3ab44d497bef9d2a059eb9
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-02-19 10:58:16 -0800

    Print all alphabets except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char qNe;
+
+	for (qNe = 'a'; qNe <= 'z'; qNe++)
+	{
+		if (qNe != 'e' && qNe != 'q')
+			putchar(qNe);
+	}
+	putchar('\n');
+	return (0);
+}

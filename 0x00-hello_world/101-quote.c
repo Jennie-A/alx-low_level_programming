506ae83deea6ae2f4519a083e086dad66dfd205d
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-02-20 11:48:08 -0800

    Unix is simple yet difficult to understand

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - the program prints to the standard error
+ * Return: 1
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}

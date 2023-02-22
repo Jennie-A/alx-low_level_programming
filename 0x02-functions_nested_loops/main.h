bbbdc803f48ec73555a7d7682f09607b15308b3a
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-02-21 17:04:28 -0800

    Print _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#ifndef main_h
+#define main_h
+
+/**
+ * File: main.h
+ *
+ * Description: Declarations for all functions I will use in the directory 0x02-functions_nested_loops
+ * For ALX functions,nested loops task
+ */
+
+int_putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+
+#endif

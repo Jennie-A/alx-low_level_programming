1b392a378fe25fa9442084b96a8fc724fa43228a
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-02-21 18:28:08 -0800

    Alphabet_printing

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,12 @@
  *
  * Return: Always 0
  */
-int main(void)
+void print_alphabet(void)
 {
 	char L;
 
 	for (L = 'a'; L <= 'z'; L++)
-		_putchar(L);
+		_putchar('L');
 	_putchar('\n');
-	return (0);
+
 }

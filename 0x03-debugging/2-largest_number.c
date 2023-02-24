50f00a19d017d15dd23002ba3390ef9d3ec5e783
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-02-23 20:19:45 -0800

    Biggest Number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -17,13 +17,13 @@ int largest_number(int a, int b, int c)
 		largest = a;
 	}
 
-	else if (b > c && b > a)
-	{
-		largest = b;
-	}
-	else if (c > b)
+	else if (a > b && c > a)
 	{
 		largest = c;
+	}
+	else if (b > c)
+	{
+		largest = b;
 	}
 
 	else

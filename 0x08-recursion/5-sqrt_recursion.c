740901a6de2d5b9796ee690069ed181b1d053052
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-03-08 20:19:41 -0800

    4-pow_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Write a function that returns the natural square root of a number
+ * @n: the number to find the sqrt
+ * if n does not have natural square root,
+ * Return: -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+

33763903e18af9507b1576d5e3faa5ad8cb5186b
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-03-11 14:07:21 -0800

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the programs prints all arguments it receives
+ * all argument should be printed including the first one
+ * only one argument per line
+ * @argc: arguments to be counted
+ * @argv: array to be counted
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i >= 1; i++)
+	printf("%d\n", argc);
+
+	return (0);
+}

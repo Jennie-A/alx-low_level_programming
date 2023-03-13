0adfb43200f6749f7a6d14bdd9f2bb24c6426be2
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-03-13 10:34:56 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - the program multiplies two numbers
+ * the prgram prints the results of the multiplication
+ * followed by a new line
+ * Return: if the program does not receive two arguments - 1
+ * if it receives two arguments - 0
+ * @argc: the number of arguments
+ * @argv: the array of pointers to the arguments
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	mul = 0;
+	mul = a * b;
+
+	printf("%d\n", mul);
+
+	return (0);
+}

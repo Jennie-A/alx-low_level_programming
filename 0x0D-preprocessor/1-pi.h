ab7525d7f4253c5086cf9e51cdfec085b2c5b891
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-03-20 14:41:24 -0700

    1-pi.h

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,10 +1,6 @@
-#include <stdio.h>
+#ifndef MACROS_H
+#define MACROS_H
 
 #define PI 3.14159265359
 
 #endif
-
-/**
- * create a header file that defines a macro named PI
- * as an abbreviation for the token 3.14159265359
- */

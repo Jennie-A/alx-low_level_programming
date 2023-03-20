5b5ec61f328ad1dfb3283e7de2c9dbed4d42ece8
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-03-20 15:03:32 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the program prints the name of the file
+ * it was compiled from followed by a new line
+ * Return: 0 success
+ */
+int main(void)
+{
+	printf("File : %s\n", __FILE__);
+
+	return (0);
+}

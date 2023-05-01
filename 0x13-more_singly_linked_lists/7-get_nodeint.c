36316b3e9009ce338c740aead304a6b3e635a38e
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-05-01 15:16:12 -0700

    get node fro m list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - write a function that returns the
+ * nth node of a listint_t linked list
+ * @head: pointer to first node
+ * @index: as an iterator
+ * Return: Null if node does not exist
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+
+	listint_t *temp;
+	unsigned int i;
+
+	temp = head;
+
+	if (head == NULL)
+	return (NULL);
+
+	for (i = 0; i < index; i++)
+	temp = temp->next;
+
+	if (temp == NULL)
+		return (NULL);
+
+	return (temp);
+}

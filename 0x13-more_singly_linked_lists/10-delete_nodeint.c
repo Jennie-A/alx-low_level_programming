328e98f574c984fcee1e0e91e7f7a634efe5f6d3
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-05-02 10:48:13 -0700

    delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,10 +22,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	return (1);
 	}
 
-	current_node = *head;
-
 	for (i = 0; i < index; i++)
 	{
+		current_node = *head;
 		current_node = current_node->next;
 		if (current_node == NULL)
 			return (-1);

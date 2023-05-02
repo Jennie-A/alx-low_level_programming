ebf33edb9e8963053ef8909371534029c28cab41
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-05-02 14:43:54 -0700

    reverse linked list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,7 +10,7 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *temp;
 
 	if (*head == NULL || (*head)->next == NULL)
-		return (*head);
+		return (NULL);
 
 	temp = reverse_listint(&((*head)->next));
 	(*head)->next->next = *head;

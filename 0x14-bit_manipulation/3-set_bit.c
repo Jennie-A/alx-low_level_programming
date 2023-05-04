9e5e338861859932bef9a18a98d06533f54d8cbc
Jennie-A <122853616+Jennie-A@users.noreply.github.com>
2023-05-04 15:42:00 -0800

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,6 +6,16 @@
  * @index: starting from 0 of the bit you want to set
  * Return: 1 on success or -1 if erroe occurs
  */
+/**
+ * set bit from 1 value to another, ie, 0 to 1
+ * check if indices are within range, if not error
+ * *n a variable that holds a number, |= take value on left side and combine with value on right using bitwise operator OR
+ * store results back in the value on left side
+ * << bitwise operator shift n  to the left side at the index
+ * i.e 00000001 becomes 000010000 if we move 1 to index 3
+ * return 1 on success
+ */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)

6dd7da71691526851dceb1f5261f45947ef90442
Jennie-A <122853616+Jennie-A@users.noreply.github.com>
2023-05-04 16:19:05 -0800

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,6 +6,13 @@
  * @index: starting from 0 of the bit you want to set
  * Return: 1 on success or -1 if an error occurs
  */
+
+/**
+ * same implementation as the previous "setting value to 1 at a given index
+ * the only difference is the introduction of the bitwise operator NOT "~" after shifting the index to the left
+ * 1UL is a suffix for unsigned long integer
+ */
+
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)

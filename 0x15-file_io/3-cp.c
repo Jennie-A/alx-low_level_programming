badaf840ac1cb6152788fc21dc44d371378c68f8
Jennifer Akyereko <jenniphermiss@gmail.com>
2023-05-25 03:30:14 -0700

    copy and paste

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,30 +15,30 @@ void check_IO_stat(int stat, int fdes, char *filename, char mode);
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, num_read = 1024, wrote, close_file_from, close_file_to;
+	int file_f, file_to, num_read = 1024, wrote, close_file_f, close_file_to;
 	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	char buffer[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "%s", "Usage: cp file_f file_to\n");
 		exit(97);
 	}
-	file_from = open(argv[1], O_RDONLY);
-	check_IO_stat(file_from, -1, argv[1], 'O');
+	file_f = open(argv[1], O_RDONLY);
+	check_IO_stat(file_f, -1, argv[1], 'O');
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
 	check_IO_stat(file_to, -1, argv[2], 'W');
 	while (num_read == 1024)
 	{
-		num_read = read(file_from, buffer, sizeof(buffer));
+		num_read = read(file_fr, buffer, sizeof(buffer));
 		if (num_read == -1)
 			check_IO_stat(-1, -1, argv[1], 'O');
 		wrote = write(file_to, buffer, num_read);
 		if (wrote == -1)
 			check_IO_stat(-1, -1, argv[2], 'W');
 	}
-	close_file_from = close(file_from);
-	check_IO_stat(close_file_from, file_from, NULL, 'C');
+	close_file_f = close(file_f);
+	check_IO_stat(close_file_f, file_f, NULL, 'C');
 	close_file_to = close(file_to);
 	check_IO_stat(close_file_to, file_to, NULL, 'C');
 	return (0);
